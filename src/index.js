4355320717ad63362e6d481eae28d3e205adcc0c
Messias <messias.freirejr@outlook.com>
2021-04-02 21:14:47 -0300

    chore: initial setup

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { ApolloProvider } from '@apollo/client';
+
+import { client } from './graphql/setup';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Rocket from './components/Rockets'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  return (
+  <ApolloProvider client={client}>
+   <React.StrictMode>
+    <Rocket rocket="Qualquer coisa aqui"/>
+   </React.StrictMode>,
+ </ApolloProvider> 
+  );
+}
+render(<App/>,document.getElementById('root'));
